c3f96d98b214a693b5b039dbd31c60a70d1939b8
Karl Leswing <lilleswing@gmail.com>
2020-04-17 10:24:01 -0400

    Fallback on bash if make doesn't exist for installing

--- devtools/jenkins.sh
+++ devtools/jenkins.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-make install
+if ! [ -x "$(command -v make)" ]; then
+  echo 'Warning Make Not Installed Using Bash'
+  bash devtools/install.sh
+else
+  make install
+fi
+
 export PATH=`pwd`/anaconda/bin:$PATH
 source activate ml_starter
 
